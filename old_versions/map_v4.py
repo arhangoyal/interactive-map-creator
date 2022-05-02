62617f5b5fdbe393fae7f85b8dce852ac4e6af7a
Arhan Goyal <goyalarhan@gmail.com>
2022-05-02 02:30:31 -0500

    final program ready

--- old_versions/map_v4.py
+++ old_versions/map_v4.py
@@ -19,26 +19,14 @@ from myfunctions import find_coordinate_cols, clean_coord_data, get_map_filter
 # main
 
 infile = input("Enter full/relative path to (CSV) dataset on system: ") # ./input/meteorite-landings.csv
-
-# clean data (meteorite data specific)
-dirty_df = pd.read_csv(infile, delimiter=",")
-filtered_mass = ~pd.isna(dirty_df["mass"])
-filtered_type = ((dirty_df["nametype"] == "Valid"))
-dirty_df = dirty_df[filtered_mass & filtered_type]
-dirty_df.drop(["GeoLocation", "nametype"], axis=1, inplace=True)
-dirty_df = dirty_df.sort_values(by="year", ascending=True)
-dirty_df["mass"] = dirty_df["mass"].div(1000) # convert gram to kilogram. This is specific to the meteorite dataset
-dirty_df.to_csv("./data/pre-cleaned-data.csv", index=False)
-
-# Find coordinate columns, rename them, and filter out data outside valid coordinate range
-find_coordinate_cols("./data/pre-cleaned-data.csv")
+find_coordinate_cols(infile)
 clean_coord_data("./data/coord-data-found.csv")
-
 
 # Plot data
 
 # open coordinate filtered data file
 clean_df = pd.read_csv("./data/coord-filtered-data.csv", delimiter=",")
+clean_df["mass"] = clean_df["mass"].div(1000) # convert gram to kilogram. This is specific to the meteorite dataset
 
 # input: filter 1 (col1) for heatmap and plot points and filter 2 (col2) for plot points only
 col1, col1_min, col1_max = get_map_filter(clean_df, "Enter name of column 1 (int/float) to filter out data: ")
@@ -59,13 +47,8 @@ map = Map(location=[0, 0], zoom_start=2, control_scale=True)
 # plot points using mask1
 for index, row in include.iterrows():
     icon = features.CustomIcon("https://i.imgur.com/YCwSiQa.png", icon_size=(53.75,66.25)) # custom icon
-    col_names = [col_name for col_name in clean_df.columns.values.tolist()]
-    info_wall = ""
-    for i, name in enumerate(col_names):
-        info_wall = info_wall + name + " " + str(row[name]) + "\n"
     Marker([row["Latitude"], row["Longitude"]],
-    popup = info_wall, 
-    icon=icon ).add_to(map)
+    popup = "Name: " + row["name"] + " Mass: " + str(row["mass"]) + " Fall: " + str(row["fall"]) + " Lat: " + str(row["Latitude"]) + " Long: " + str(row["Longitude"]), icon=icon ).add_to(map)
 
 # plot heatmap using mask2
 mask2 = mask2[['Latitude', 'Longitude']]
